8992465308b31e2dab177aa68b08873b15515b5b
uday <udaykiran9147@gmail.com>
2026-03-06 10:32:44 +0530

    feat: Add JWT authentication middleware and extend Express Request type to include authenticated user data.

---
+++ locaura-backend/src/middlewares/auth_middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { AuthUser } from '../types/express';
+
+export const auth_middleware = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ success: false, message: 'Unauthorized' });
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as any;
+        req.user = decoded as AuthUser;
+        next();
+    } catch (error) {
+        res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
+}
