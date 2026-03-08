7a5faeae7d5fe4c8d6f69a2a374304c365ca5033
uday <udaykiran9147@gmail.com>
2026-03-08 10:23:02 +0530

    docs: add docs to express types

--- locaura-backend/src/types/express.d.ts
+++ locaura-backend/src/types/express.d.ts
@@ -5,6 +5,16 @@ export interface AuthUser {
   role: string;
 }
 
+/**
+ * Augments the Express Request interface to include an optional authenticated user.
+ * 
+ * @remarks
+ * This declaration extends the global Express namespace to add user information
+ * to incoming requests after authentication middleware has processed them.
+ * 
+ * @property {AuthUser} [user] - The authenticated user object, if the request has been authenticated.
+ * Will be undefined for unauthenticated requests. Can be used to extend Express objects like Request
+ */
 declare global {
   namespace Express {
     interface Request {
